a0aa3d6926c3181c26424aa0c41850002e24bcda
Sai Ganesh <117970120+AKAspidey01@users.noreply.github.com>
2025-03-07 11:06:11 +0530

    side menu fix

--- src/utils/SideMenuWrapper.jsx
+++ src/utils/SideMenuWrapper.jsx
@@ -4,9 +4,7 @@ import SideBar from "../Components/Navigation/SideBar/SideBar";
 
 export default function SideMenuWrapper() {
     const location = useLocation();
-    // const isPageNotFoundPage = location.pathname === '/' || location.pathname === '/About-Me' || location.pathname === '/LearnHtml' || 
-    // location.pathname === '/LearnTailwind' ||  location.pathname === '/LearnCss' || location.pathname.includes('LearnReact') || location.pathname.includes('LearnAngular');
-    const shouldShowSideMenu =  location.pathname !== '/login' ;
+    const shouldShowSideMenu = location.pathname.replace(/\/$/, '') !== '/login';
     
     return (
         shouldShowSideMenu ? <SideBar/> : null
